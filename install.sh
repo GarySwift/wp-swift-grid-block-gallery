5c0324d36e2116476744727b0be8c40515db1d8c
Gary Swift <garyswiftmail@gmail.com>
2018-06-05 13:51:46 +0100

    Added scss file

--- install.sh
+++ install.sh
@@ -13,6 +13,8 @@ then
 fi
 mv page-gallery.php $theme_path"/page-templates/page-gallery.php"
 mv magnific-popup.js $theme_path"/src/assets/js/lib/magnific-popup.js"
+mv _gallery.scss $theme_path"/src/assets/scss/_gallery.scss"
+
 printf "Do wish to install magnific-popup using npm? [y/N] "
 read verfiy
 # Check result
@@ -22,11 +24,18 @@ then
 	echo "npm install magnific-popup --save"
 	npm install magnific-popup --save
 	echo
-	echo "Please add the following imports to src/assets/js/app.js"
-	echo "import '../../../node_modules/magnific-popup/dist/jquery.magnific-popup.min';"
-	echo "import './lib/magnific-popup';"
-	echo
-	echo "Please add the following imports to src/assets/scss/app.scss"
-	echo "@import '../../../node_modules/magnific-popup/dist/magnific-popup';"
+	echo "import '../../../node_modules/magnific-popup/dist/jquery.magnific-popup.min';" >> $theme_path"/src/assets/js/app.js"
+	echo "import './lib/magnific-popup';" >> $theme_path"/src/assets/js/app.js"
+	# echo "Please add the following imports to src/assets/js/app.js"
+	# echo "import '../../../node_modules/magnific-popup/dist/jquery.magnific-popup.min';"
+	# echo "import './lib/magnific-popup';"
+	# echo
+	# echo "Please add the following import to src/assets/scss/app.scss"
+	# echo "@import '../../../node_modules/magnific-popup/dist/magnific-popup';"
+	echo "@import '../../../node_modules/magnific-popup/dist/magnific-popup';" >> $theme_path"/src/assets/scss/app.scss"
 	echo
 fi
+echo "@import 'gallery';" >> $theme_path"/src/assets/scss/app.scss"
+# echo "Please add the following import to src/assets/scss/app.scss"
+# echo "@import 'gallery';"
+echo
